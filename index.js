1849e6482a2aa2c3f0f9e6a0dd73a5fc1aea1061
Andreseag <castrogomezand@gmail.com>
2025-01-29 15:53:52 -0500

    feat: add new routes :sparkles:

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola desde la nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 200,
+    },
+  ]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
