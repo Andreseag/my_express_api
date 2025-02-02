5b65c3e417b0c9cd13bec4dc46e90b317ea7692a
Andreseag <castrogomezand@gmail.com>
2025-02-01 21:02:14 -0500

    feat: add routes to users :sparkles:

--- routes/users.router.js
+++ routes/users.router.js
@@ -15,4 +15,38 @@ router.get('/users', (req, res) => {
   }
 });
 
+// GET /users/:userId
+router.get('/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  res.json({
+    userId,
+    name: `User ${userId}`,
+  });
+});
+
+// POST /users
+router.post('/users', (req, res) => {
+  res.json({
+    message: 'POST /users',
+  });
+});
+
+// PUT /users/:userId
+router.put('/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  res.json({
+    userId,
+    message: 'PUT /users',
+  });
+});
+
+// DELETE /users/:userId
+router.delete('/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  res.json({
+    userId,
+    message: 'DELETE /users',
+  });
+});
+
 module.exports = router;
