abff27272db01b95f8437e5a7bb0ec675fed909d
Andreseag <castrogomezand@gmail.com>
2025-02-10 21:26:06 -0500

    feat: create all methods to department service :sparkles:

--- api/services/deparment.service.js
+++ api/services/deparment.service.js
@@ -18,4 +18,50 @@ class DepartmentService {
 
     this.departments = departments;
   }
+
+  getDepartments() {
+    return this.departments;
+  }
+
+  getDepartmentById(id) {
+    if (!this.departments.some((department) => department.id === id)) {
+      throw boom.notFound('Department not found');
+    }
+
+    return this.departments.find((department) => department.id === id);
+  }
+
+  createDepartment(department) {
+    const newDepartment = {
+      id: faker.database.mongodbObjectId(),
+      ...department,
+    };
+    this.departments.push(newDepartment);
+    return newDepartment;
+  }
+
+  updateDepartment(id, data) {
+    const index = this.departments.findIndex(
+      (department) => department.id === id
+    );
+    if (index === -1) {
+      throw boom.notFound('Department not found');
+    }
+    this.departments[index] = {
+      ...this.departments[index],
+      ...data,
+    };
+    return this.departments[index];
+  }
+
+  deleteDepartment(id) {
+    const index = this.departments.findIndex(
+      (department) => department.id === id
+    );
+    if (index === -1) {
+      throw boom.notFound('Department not found');
+    }
+    this.departments.splice(index, 1);
+    return { id };
+  }
 }
