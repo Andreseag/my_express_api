15228e1ee4400bd8bf5aaf20f5242cb2682581b2
Andreseag <castrogomezand@gmail.com>
2025-03-31 17:13:58 -0500

    feat: create User schema :sparkles:

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -38,7 +38,7 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM products';
-    const [data, metadata] = await sequelize.query(query);
+    const [data] = await sequelize.query(query);
     return {
       data,
     };
